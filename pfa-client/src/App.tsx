471d91751933d9dda8f7d997af40f942d5a9a70b
Ashiq <mizanur.r.ashiq@gmail.com>
2024-05-24 01:13:49 +0600

    add pfm client

---
+++ pfa-client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function App() {
+  return (
+      <>
+          <h1 className="text-3xl font-bold text-red-600">
+            PFM - Personal Finance Manager
+          </h1>
+      </>
+  );
+}
+
+export default App;
