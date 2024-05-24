152d825dde19088190c2ee04b2f535fb84d5e44b
Ashiq <mizanur.r.ashiq@gmail.com>
2024-05-24 16:43:29 +0600

    some modification

--- src/main/java/mra_pfm/pfm/controller/user/UserController.java
+++ src/main/java/mra_pfm/pfm/controller/user/UserController.java
@@ -1,13 +1,36 @@
 package mra_pfm.pfm.controller.user;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import mra_pfm.pfm.domain.user.User;
+import mra_pfm.pfm.service.user.UserService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@RestController("/users")
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
 
-    Map getAllUsers() {
-        return null;
+    private final UserService userService;
+
+    @GetMapping()
+    public Map getUsers() {
+        return userService.getUsers();
+    }
+
+    @PostMapping("/create")
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @PutMapping("/update")
+    public User updateUser(@RequestBody User user) {
+        return userService.updateUser(user);
+    }
+
+    @DeleteMapping("/delete/{username}")
+    public void deleteUser(@PathVariable String username) {
+        userService.deleteUser(username);
     }
 }
